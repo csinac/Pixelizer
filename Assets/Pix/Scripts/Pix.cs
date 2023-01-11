bb79656cc84deb9aab0bf95138ae4f7fd2d6df26
Ali Unal <0033484@gmail.com>
2023-01-11 16:26:46 +0300

    Pix adjusted

--- Assets/Pix/Scripts/Pix.cs
+++ Assets/Pix/Scripts/Pix.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace AngryKoala.Pixelization
 {
+    [SelectionBase]
     public class Pix : MonoBehaviour
     {
         [SerializeField] private MeshRenderer pixMeshRenderer;
@@ -10,15 +11,28 @@ namespace AngryKoala.Pixelization
         [SerializeField] private Color color;
         public Color Color => color;
 
-        public void SetColor(Color color)
-        {
-            this.color = color;
-        }
+        private Color currentColor;
 
         private void Start()
         {
             pixMeshRenderer.material.color = color;
             pixMeshRenderer.material.SetFloat("_Glossiness", 0f);
+
+            currentColor = color;
+        }
+
+        private void Update()
+        {
+            if(currentColor != color)
+            {
+                pixMeshRenderer.material.color = color;
+                currentColor = color;
+            }
+        }
+
+        public void SetColor(Color color)
+        {
+            this.color = color;
         }
     }
 }
