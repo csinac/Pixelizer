782d2401cbcd03da133360ae7b9fc71504d99ac9
Ali Unal <0033484@gmail.com>
2023-03-01 02:30:28 +0300

    Colorizer adjusted

--- Assets/Pixelization/Colorizer/Scripts/Colorizer.cs
+++ Assets/Pixelization/Colorizer/Scripts/Colorizer.cs
@@ -12,13 +12,15 @@ namespace AngryKoala.Pixelization
         [SerializeField] private ColorPalette colorPalette;
         public ColorPalette ColorPalette => colorPalette;
 
+        [SerializeField] private int extractColorPaletteColorCount;
+
         private enum ColorizationStyle { Replace, ReplaceWithOriginalSaturation, ReplaceWithOriginalValue }
         [SerializeField] private ColorizationStyle colorizationStyle;
 
         private enum ReplacementStyle { ReplaceUsingHue, ReplaceUsingSaturation, ReplaceUsingValue }
         [SerializeField] private ReplacementStyle replacementStyle;
 
-        [SerializeField] private int extractColorPaletteColorCount;
+        [SerializeField] private bool useColorGroups;
 
         public static UnityAction OnColorize;
 
@@ -36,18 +38,25 @@ namespace AngryKoala.Pixelization
                 return;
             }
 
+            List<Color> colorGroupColors = new List<Color>();
+
+            if(useColorGroups)
+            {
+                colorGroupColors = GetColorPalette(colorPalette.Colors.Count);
+            }
+
             for(int i = 0; i < pixelizer.PixCollection.Length; i++)
             {
                 switch(colorizationStyle)
                 {
                     case ColorizationStyle.Replace:
-                        pixelizer.PixCollection[i].SetColor(GetClosestColorizerColor(pixelizer.PixCollection[i].Color));
+                        pixelizer.PixCollection[i].SetColor(GetClosestColorizerColor(pixelizer.PixCollection[i].Color, colorPalette.Colors));
                         break;
 
                     case ColorizationStyle.ReplaceWithOriginalSaturation:
                         {
                             Color originalColor = pixelizer.PixCollection[i].Color;
-                            Color adjustedColor = GetClosestColorizerColor(originalColor);
+                            Color adjustedColor = GetClosestColorizerColor(originalColor, colorPalette.Colors);
 
                             float originalHue, originalSaturation, originalValue;
                             float hue, saturation, value;
@@ -64,7 +73,7 @@ namespace AngryKoala.Pixelization
                     case ColorizationStyle.ReplaceWithOriginalValue:
                         {
                             Color originalColor = pixelizer.PixCollection[i].Color;
-                            Color adjustedColor = GetClosestColorizerColor(originalColor);
+                            Color adjustedColor = GetClosestColorizerColor(originalColor, colorPalette.Colors);
 
                             float originalHue, originalSaturation, originalValue;
                             float hue, saturation, value;
@@ -84,7 +93,7 @@ namespace AngryKoala.Pixelization
             OnColorize?.Invoke();
         }
 
-        private Color GetClosestColorizerColor(Color color)
+        private Color GetClosestColorizerColor(Color color, List<Color> colorizerColors)
         {
             float hue, saturation, value;
             Color.RGBToHSV(color, out hue, out saturation, out value);
@@ -96,7 +105,7 @@ namespace AngryKoala.Pixelization
             switch(replacementStyle)
             {
                 case ReplacementStyle.ReplaceUsingHue:
-                    foreach(var colorizerColor in colorPalette.Colors)
+                    foreach(var colorizerColor in colorizerColors)
                     {
                         Vector3 colorHue = new Vector3(color.r, color.g, color.b);
                         Vector3 colorizerColorHue = new Vector3(colorizerColor.r, colorizerColor.g, colorizerColor.b);
@@ -112,7 +121,7 @@ namespace AngryKoala.Pixelization
                     break;
 
                 case ReplacementStyle.ReplaceUsingSaturation:
-                    foreach(var colorizerColor in colorPalette.Colors)
+                    foreach(var colorizerColor in colorizerColors)
                     {
                         float colorizerHue, colorizerSaturation, colorizerValue;
                         Color.RGBToHSV(colorizerColor, out colorizerHue, out colorizerSaturation, out colorizerValue);
@@ -127,7 +136,7 @@ namespace AngryKoala.Pixelization
                     }
                     break;
                 case ReplacementStyle.ReplaceUsingValue:
-                    foreach(var colorizerColor in colorPalette.Colors)
+                    foreach(var colorizerColor in colorizerColors)
                     {
                         float colorizerHue, colorizerSaturation, colorizerValue;
                         Color.RGBToHSV(colorizerColor, out colorizerHue, out colorizerSaturation, out colorizerValue);
@@ -148,59 +157,14 @@ namespace AngryKoala.Pixelization
 
         public void ExtractColorPalette()
         {
-            int iterationCount = 10;
-
-            List<Color> pixels = new List<Color>();
-            foreach(var pix in pixelizer.PixCollection)
-            {
-                pixels.Add(pix.Color);
-            }
-            int pixelCount = pixels.Count;
-
-            Color[] centroids = new Color[extractColorPaletteColorCount];
-            for(int i = 0; i < extractColorPaletteColorCount; i++)
-            {
-                centroids[i] = pixels[Random.Range(0, pixelCount)];
-            }
-
-            for(int i = 0; i < iterationCount; i++)
-            {
-                int[] nearestCentroidIndices = new int[pixelCount];
-
-                for(int j = 0; j < pixelCount; j++)
-                {
-                    float nearestDistance = float.MaxValue;
-                    int nearestCentroidIndex = 0;
-                    for(int k = 0; k < extractColorPaletteColorCount; k++)
-                    {
-                        Vector3 colorA = new Vector3(pixels[j].r, pixels[j].g, pixels[j].b);
-                        Vector3 colorB = new Vector3(centroids[k].r, centroids[k].g, centroids[k].b);
-
-                        float distance = Vector3.Distance(colorA, colorB);
-                        if(distance < nearestDistance)
-                        {
-                            nearestDistance = distance;
-                            nearestCentroidIndex = k;
-                        }
-                    }
-                    nearestCentroidIndices[j] = nearestCentroidIndex;
-                }
-
-                for(int j = 0; j < extractColorPaletteColorCount; j++)
-                {
-                    var pixelsInCluster =
-                        from pixelIndex in Enumerable.Range(0, pixelCount)
-                        where nearestCentroidIndices[pixelIndex] == j
-                        select pixels[pixelIndex];
-
-                    if(pixelsInCluster.Any())
-                    {
-                        centroids[j] = new Color(pixelsInCluster.Average(c => c.r), pixelsInCluster.Average(c => c.g), pixelsInCluster.Average(c => c.b));
-                    }
-                }
-            }
-
 #if UNITY_EDITOR
+            if(extractColorPaletteColorCount <= 0)
+            {
+                Debug.LogWarning("Color palette color count must be greater than 0");
+                return;
+            }
+
+            List<Color> centroids = GetColorPalette(extractColorPaletteColorCount);
 
             ColorPalette newColorPalette = ScriptableObject.CreateInstance<ColorPalette>();
 
@@ -215,6 +179,63 @@ namespace AngryKoala.Pixelization
 
             colorPalette = newColorPalette;
 #endif
+        }
+
+        private List<Color> GetColorPalette(int colorCount)
+        {
+            int iterationCount = 10;
+
+            List<Color> pixels = new List<Color>();
+            foreach(var pix in pixelizer.PixCollection)
+            {
+                pixels.Add(pix.Color);
+            }
+            int pixelCount = pixels.Count;
+
+            Color[] centroids = new Color[colorCount];
+            for(int i = 0; i < colorCount; i++)
+            {
+                centroids[i] = pixels[Random.Range(0, pixelCount)];
+            }
+
+            for(int i = 0; i < iterationCount; i++)
+            {
+                int[] nearestCentroidIndices = new int[pixelCount];
+
+                for(int j = 0; j < pixelCount; j++)
+                {
+                    float nearestDistance = float.MaxValue;
+                    int nearestCentroidIndex = 0;
+                    for(int k = 0; k < colorCount; k++)
+                    {
+                        Vector3 colorA = new Vector3(pixels[j].r, pixels[j].g, pixels[j].b);
+                        Vector3 colorB = new Vector3(centroids[k].r, centroids[k].g, centroids[k].b);
+
+                        float distance = Vector3.Distance(colorA, colorB);
+                        if(distance < nearestDistance)
+                        {
+                            nearestDistance = distance;
+                            nearestCentroidIndex = k;
+                        }
+                    }
+                    nearestCentroidIndices[j] = nearestCentroidIndex;
+                }
+
+                for(int j = 0; j < colorCount; j++)
+                {
+                    var pixelsInCluster =
+                        from pixelIndex in Enumerable.Range(0, pixelCount)
+                        where nearestCentroidIndices[pixelIndex] == j
+                        select pixels[pixelIndex];
+
+                    if(pixelsInCluster.Any())
+                    {
+                        centroids[j] = new Color(pixelsInCluster.Average(c => c.r), pixelsInCluster.Average(c => c.g), pixelsInCluster.Average(c => c.b));
+                    }
+                }
+            }
+
+            return centroids.ToList();
         }
 
         public void ComplementColors()
